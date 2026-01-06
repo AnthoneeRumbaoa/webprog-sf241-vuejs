991d7d1d6840a54e0b7fac5e2056938f9f337aa4
Anthonee Jhel Rumbaoa <amrumbaoa@student.apc.edu.ph>
2026-01-06 15:38:54 +0800

    still trying to fix

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,9 +1,19 @@
-  const app = Vue.createApp({
-   data() {
-    return {
-     message: "Hello Ramen!"
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            archive: [
+                "1.jpg", 
+                "2.jpg", 
+                "3.jpg", 
+                "4.jpg", 
+                "5.jpg", 
+                "6.jpg", 
+                "7.jpg", 
+                "8.jpg", 
+                "9.jpg"
+            ]
+        }
     }
-   }
-  })
-  
-  app.mount('#app')
+}).mount('#app');
