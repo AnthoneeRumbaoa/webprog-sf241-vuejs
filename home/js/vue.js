4c944d313ebfd8786ab870ddbc2c366c5aebcdcf
Anthonee Jhel Rumbaoa <amrumbaoa@student.apc.edu.ph>
2026-01-06 16:09:41 +0800

    fixes everything back to normal

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,19 +1,9 @@
-const { createApp } = Vue;
-
-createApp({
-    data() {
-        return {
-            archive: [
-                "1.jpg", 
-                "2.jpg", 
-                "3.jpg", 
-                "4.jpg", 
-                "5.jpg", 
-                "6.jpg", 
-                "7.jpg", 
-                "8.jpg", 
-                "9.jpg"
-            ]
-        }
+  const app = Vue.createApp({
+   data() {
+    return {
+     message: "Hella World!"
     }
-}).mount('#app');
+   }
+  })
+  
+  app.mount('#app')
