caf6b098ef656cd84e5b600194f33251b09b8c00
Anthonee Jhel Rumbaoa <amrumbaoa@student.apc.edu.ph>
2026-01-13 06:52:42 +0000

    Task 03 Create a Clean Project

---
+++ firstsfc/src/components/FoodItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Ramen',
+      message: 'I like ramen'
+    }
+  }
+};
+</script>
+
+<style></style>
