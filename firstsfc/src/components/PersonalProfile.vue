55d39ed3ec9395893f0b3b2f4b2c66b0c0c11ed3
Anthonee Jhel Rumbaoa <amrumbaoa@student.apc.edu.ph>
2026-01-16 15:14:59 +0800

    fixing again

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,50 +1,54 @@
 <template>
-<h1 style="text-align: center;">Personal Profile Web Page - Anthonee Rumbaoa</h1>
+  <div class="main-wrapper">
+    <h1 style="text-align: center;">Personal Profile Web Page - Anthonee Rumbaoa</h1>
 
-<div class="container">
-    
-<div class="card">
-    <h3>Course</h3>
+    <div class="container">
+      
+      <div class="card">
+        <h3>Course</h3>
         <p>Bachelor in Science Computer Science specializing Cybersecurity and Forensic - Asia Pacific College</p>
-            </div>
-    
-<div class="card">
-    <h3>About Me</h3>
-        <p>Hello! <div id="app"> {{ message }} </div> and pagod na ako.</p>
-            </div>
+      </div>
 
-<div class="card">
-    <h3>IT Experience</h3>
+      <div class="card">
+        <h3>About Me</h3>
+        <p>Hello! {{ message }} and pagod na ako.</p>
+      </div>
+
+      <div class="card">
+        <h3>IT Experience</h3>
         <p>Mag open ng computer</p>
-            </div>
+      </div>
 
-<div class="card">
-<h3>Hobbies & Interests</h3>
-    <ul>
-        <p>Loves to play sports and videogames</p>
-        <p>A fan of R&B music</p>
-        <p>Laging pagod</p>
-            </ul>
-                </div>
+      <div class="card">
+        <h3>Hobbies & Interests</h3>
+        <ul>
+          <li>Loves to play sports and videogames</li>
+          <li>A fan of R&B music</li>
+          <li>Laging pagod</li>
+        </ul>
+      </div>
 
-<div class="card image-card">
-    <div id="app2">
-
-  <div>
-    <img v-for="x in manyFoods" v-bind:src="x">
-  </div>
-
-</div>
+      <div class="card image-card">
+        <div>
+          <img 
+            v-for="(image, index) in manyFoods" 
+            :key="index" 
+            :src="image" 
+            alt="Food item"
+            style="width: 100px; margin: 5px;" 
+          />
         </div>
+      </div>
 
-<div class="card">
-    <h3>Goals In Life / Dream</h3>
+      <div class="card">
+        <h3>Goals In Life / Dream</h3>
         <p>Experience 10+hrs of sleep everyday</p>
-            </div>
+      </div>
 
-</div>
+    </div>
+  </div>
 </template>
- 
+
 <script></script>
- 
+
 <style src="./css/css-grid.css"></style>
