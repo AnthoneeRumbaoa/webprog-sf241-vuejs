82459f8e920ead4a56c14b0c593225497e4c3ac4
Anthonee Jhel Rumbaoa <amrumbaoa@student.apc.edu.ph>
2026-01-18 00:34:26 +0800

    BRINGING MY MIDTERM PROJ

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,9 +1,19 @@
-  const app = Vue.createApp({
-   data() {
+const app = Vue.createApp({
+  data() {
     return {
-     message: "I'm Aj!"
+      TheArchive: [
+        '1.jpg',
+        '2.jpg',
+        '3.jpg',
+        '4.jpg',
+        '5.jpg',
+        '6.jpg',
+        '7.jpg',
+        '8.jpg',
+        '9.jpg'
+      ]
     }
-   }
-  })
-  
-  app.mount('#app')
+  }
+})
+
+app.mount('#app')
